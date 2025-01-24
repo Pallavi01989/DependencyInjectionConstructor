827438cda287a0c22e5c75381184e41baed4284b
sonva <sonva@Rahul>
2025-01-24 12:15:34 +0530

    add project by dev1

---
+++ DependencyInjectionConstructor/src/com/primitive/Student.java
@@ -0,0 +1,22 @@
+package com.primitive;
+
+public class Student {
+
+	private int rollno;
+	private String name;
+	private String addr;
+	
+	public Student(int rollno,String name,String addr)
+	{
+		super();
+		this.rollno=rollno;
+		this.name=name;
+		this.addr=addr;
+	}
+	@Override
+	public String toString() {
+		return "Student [rollno=" + rollno + ", name=" + name + ", addr=" + addr + ", getClass()=" + getClass()
+				+ ", hashCode()=" + hashCode() + ", toString()=" + super.toString() + "]";
+		
+	}
+}
